b283d9f3dbad0a4f8d61d0e93a0de84d7eda53c1
capitancuro <127172825+capitancuro@users.noreply.github.com>
2024-11-12 00:21:30 -0800

    Update AssetsManager.java

--- app/src/main/java/org/plot_four_app/AssetsManager.java
+++ app/src/main/java/org/plot_four_app/AssetsManager.java
@@ -1,17 +1,25 @@
 package org.plot_four_app;
 
-import javafx.scene.media.Media;
-import javafx.scene.text.Font; 
-
 import java.io.File;
 
+import javafx.scene.media.Media;
+import javafx.scene.image.Image;
+import javafx.scene.text.Font; 
+
+
 public class AssetsManager {
+	public Image getImage() {
+		return new Image(
+			new File(
+				new File("").getAbsolutePath() + 
+				"/src/main/resources/Plot_Four.png").toURI().toString());
+	}
 	
 	public Font getFont(double size) {
 		return Font.loadFont(
             new File(
                 new File("").getAbsolutePath() + 
-                "/src/main/resources/fonts/PressStart2P-Regular.ttf").toURI().toString(),
+                "/src/main/resources/PressStart2P-Regular.ttf").toURI().toString(),
                 size);
 	}
 
@@ -19,6 +27,6 @@ public class AssetsManager {
 		return new Media(
 			new File(
 				new File("").getAbsolutePath() + 
-				"/src/main/resources/audio/drop_sound.m4a").toURI().toString());
+				"/src/main/resources/drop_sound.m4a").toURI().toString());
 	}
 }
